f3a73632c7cdba520df42cd5bb447b72489d5779
daniil-konovalenko <daniil-konovalenko@users.noreply.github.com>
2021-01-06 16:17:15 +0000

    Upload documentation for master

---
+++ master/doc/implementors/serde/ser/trait.Serialize.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["pyo3"] = [{"text":"impl&lt;T&gt; Serialize for Py&lt;T&gt; <span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;T: Serialize + PyClass,&nbsp;</span>","synthetic":false,"types":[]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
